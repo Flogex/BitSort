3f34a54e04a517187256cdeacb4582dc4a9ef21d
Florian G <gerlinghoff@live.de>
2016-12-04 11:46:06 +0100

    Added BitSort and BitArrayConverter

---
+++ BitArrayConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Flogex.SortingAllgorithms.BitSortTools
+{
+    class BitArrayConverter
+    {
+        public static int[] ConvertToIntArray (BitArray arr)
+        {
+            int[] valuesAsInt = new int[arr.Length / 8];
+            for (int index = 0; index < valuesAsInt.Length; index++)
+            {
+                for (int bit = 0; bit <= 7; bit++)
+                {
+                    valuesAsInt[index] += arr[bit + (index * 8)] ? (int) Math.Pow(2, bit) : 0;
+                }
+            }
+            return valuesAsInt;
+        }
+
+        public static string[] ConvertToStringArray (BitArray arr)
+        {
+            string[] binaryNumbers = new string[arr.Length / 8];
+            StringBuilder stringBuilder = new StringBuilder(8, 8);
+            for (int number = 0; number < binaryNumbers.Length; number++)
+            {
+                for (int bit = 7; bit >= 0; bit--)
+                {
+                    stringBuilder.Append(arr[bit + (number * 8)] ? "1" : "0");
+                }
+                binaryNumbers[number] = stringBuilder.ToString();
+                stringBuilder.Clear();
+            }
+            return binaryNumbers;
+        }
+    }
+}
---
+++ BitSort.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace Flogex.SortingAllgorithms
+{
+    class BitSort
+    {
+        ///!Please read documentation of BitArray Class: https://msdn.microsoft.com/en-us/library/system.collections.bitarray(v=vs.110).aspx
+        public static BitArray Sort (BitArray unsortedArray, int position)
+        {
+            if (unsortedArray.Length == 0 || position < 0)
+            {
+                return unsortedArray;
+            }
+            BitArray numbersWithZeroBit = new BitArray(0);
+            BitArray numbersWithOneBit = new BitArray(0);
+            BitArray sortedArray = new BitArray(unsortedArray.Length);
+
+            //Check bit of each number on current positon
+            for (int i = position; i < unsortedArray.Length; i += 8)
+            {
+                //Add all bits of the number to one of the two arrays
+                if (unsortedArray.Get(i))
+                {
+                    //Decrease to zero or a multiple of 8
+                    int indexOfFirstBit = i - i % 8;
+                    int oldArrayLength = numbersWithOneBit.Length;
+                    numbersWithOneBit.Length += 8;
+                    for (int indexOfBit = indexOfFirstBit; indexOfBit <= indexOfFirstBit + 7; indexOfBit++)
+                    {
+                        numbersWithOneBit[oldArrayLength + indexOfBit - indexOfFirstBit] = unsortedArray[indexOfBit];
+                    }
+                }
+                else
+                {
+                    int indexOfFirstBit = i - i % 8;
+                    int oldArrayLength = numbersWithZeroBit.Length;
+                    numbersWithZeroBit.Length += 8;
+                    for (int indexOfBit = indexOfFirstBit; indexOfBit <= indexOfFirstBit + 7; indexOfBit++)
+                    {
+                        numbersWithZeroBit[oldArrayLength + indexOfBit - indexOfFirstBit] = unsortedArray[indexOfBit];
+                    }
+                }
+            }
+            
+            sortedArray = AddRange(Sort(numbersWithZeroBit, --position), Sort(numbersWithOneBit, position));
+            return sortedArray;
+        }
+
+        private static BitArray AddRange (BitArray a, BitArray b)
+        {
+            int oldLength = a.Length;
+            a.Length += b.Length;
+            for (int i = oldLength; i < a.Length; i++)
+            {
+                a[i] = b[i - oldLength];
+            }
+            return a;
+        }
+    }
+}
